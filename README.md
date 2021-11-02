c71de800cfecbe530fac1a8d17f1e02572ee679c
baiyi <baiyi@accesscorporate.com.cn>
2021-11-02 15:50:28 +0800

    feat: readme更新/原型

---
+++ README.md
@@ -0,0 +1,24 @@
+<!--
+ * @Date: 2021-11-01 14:55:39
+ * @LastEditors: Cosima
+ * @LastEditTime: 2021-11-01 14:57:45
+ * @FilePath: /js-test/README.md
+-->
+# Javascript代码示例
+> 收集各种乱七八糟实现
+## 目前用到的技术
+> Webpack5 + ES6 主要还是以原生为主 也可以作为一个webpack5练手配置的项目
+
+## 包含的模块
+- 未完待续，持续增加中。。。
+
+## Build Step
+
+# install dependencies
+npm install
+
+# serve with hot reload at 127.0.0.1:8888
+npm run start
+
+# build without server
+npm run build
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /*
  * @Date: 2021-10-20 15:57:06
  * @LastEditors: Cosima
- * @LastEditTime: 2021-11-01 14:42:25
+ * @LastEditTime: 2021-11-02 15:36:12
  * @FilePath: /js-test/src/index.js
  */
 import _ from 'lodash'
@@ -9,11 +9,38 @@ import _ from 'lodash'
 function component() {
   const element = document.createElement('div');
 
-  // lodash（目前通过一个 script 引入）对于执行这一行是必需的
-  // lodash 在当前 script 中使用 import 引入
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
   return element;
 }
 
 document.body.appendChild(component());
+
+// ------------原型链------------
+
+function People() {
+  this.type = '1-1'
+}
+function Person() {
+  this.type = '1'
+}
+
+// prototype是构造函数的方法，指向原型对象 {Object}
+// __proto__是实例对象的属性，指向原型对象 {Object}
+// constructor是对象的属性，指向构造函数 People() 
+
+People.prototype = new Person()
+
+// 实例对象
+const people = new People()
+const person = new Person()
+
+console.log(people, 'people的实例');
+console.log(People.prototype, 'People原型对象')
+console.log(people.__proto__, 'people原型对象');
+console.log(people.__proto__.constructor, 'people的构造函数');
+console.log(people.__proto__ === People.prototype, '------');
+
+// ----------------原型链END---------------------
+
+
